c53ff43d090c5b5d54de187ce579b678f2419d57
saideshith1905 <168267308+saideshith1905@users.noreply.github.com>
2024-04-28 07:32:55 +0000

    test for cli

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # functions-from-zero
 
 [![Python application test with Github Actions](https://github.com/saideshith1905/functions-from-zero/actions/workflows/main.yml/badge.svg)](https://github.com/saideshith1905/functions-from-zero/actions/workflows/main.yml)
+
+
---
+++ wikibot.py
@@ -0,0 +1,12 @@
+from mylib.bot import scrape
+import click
+
+@click.command()
+@click.option('--name',help='web page we want to scrape')
+def cli(name):
+    result = scrape(name)
+    click.echo(click.style(f'{result}:',fg='white',bg='black'))
+
+if __name__ == '__main__':
+    cli()
+
