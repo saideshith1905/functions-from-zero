cba657363d0e53284d651664ebcc63c7966fe6a9
saideshith1905 <168267308+saideshith1905@users.noreply.github.com>
2024-04-28 07:45:13 +0000

    adding working code

--- mystatements.py
+++ mystatements.py
@@ -1,10 +1,10 @@
-my_list = ["apple","bananna","grape"]
-my_dict = {"drink": "coffee","milk":"whole"}
+my_list = ["apple", "bananna", "grape"]
+my_dict = {"drink": "coffee", "milk": "whole"}
 
 for i in my_list:
     print(f"my favourite to eat {i}")
 
-for _,value in my_dict.items():
+for _, value in my_dict.items():
     print(f"my favourite to drink {value}")
 
-#my_dict = my_dict
+# my_dict = my_dict
--- wikibot.py
+++ wikibot.py
@@ -1,12 +1,13 @@
 from mylib.bot import scrape
 import click
 
+
 @click.command()
-@click.option('--name',help='web page we want to scrape')
+@click.option("--name", help="web page we want to scrape")
 def cli(name):
     result = scrape(name)
-    click.echo(click.style(f'{result}:',fg='white',bg='black'))
+    click.echo(click.style(f"{result}:", fg="white", bg="black"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
-
